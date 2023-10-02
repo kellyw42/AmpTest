48aad26f84cbae8064f82ed805222cb3112f7693
Wayne <w.kelly@qut.edu.au>
2023-10-02 19:07:59 +1000

    Initial Commit

---
+++ AmpTest/AmpTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmpTestApp.swift
+//  AmpTest
+//
+//  Created by Wayne Kelly on 2/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AmpTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
