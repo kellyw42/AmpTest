8f493b896bc504be3eebc44709de9f8367a1240a
Wayne <w.kelly@qut.edu.au>
2023-10-02 19:36:59 +1000

    First

--- AmpTest/AmpTestApp.swift
+++ AmpTest/AmpTestApp.swift
@@ -5,13 +5,30 @@
 //  Created by Wayne Kelly on 2/10/2023.
 //
 
+import Amplify
+import AmplifyPlugins
 import SwiftUI
 
 @main
 struct AmpTestApp: App {
+    
+    init() {
+        configureAmplify()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    private func configureAmplify() {
+        do {
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.configure()
+            print("Successful")
+        } catch {
+            print("Fail", error)
         }
     }
 }
